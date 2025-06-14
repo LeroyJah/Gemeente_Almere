f4548c1fcde0c57d30b00661ebde722f35f304a2
Jahmil <roy_animo@hotmail.com>
2025-06-14 19:35:26 +0200

    created layout2 for fullscreen hero

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,14 @@
-<x-layout.layout>
-    <x-slot:heading>
-        Welkom bij de Lief&Leed aanvraag assistant
-    </x-slot:heading>
+<x-layout.layout2>
+    <div class="flex justify-center">
+        <div class="mb-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+            </svg>
+        </div>
+        <x-slot:heading>
+            Welkom bij de Lief&Leed aanvraag assistent
+        </x-slot:heading>
+    </div>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
@@ -33,10 +40,10 @@
             @endif
         </header>
         <div class="flex justify-center">
-            <img src="{{ asset('images/stadhuis.jpg') }}" alt="">
+            <img src="{{ asset('images/stadhuis.jpg') }}" alt="" class="h-140 w-3/4 rounded">
         </div>
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
     </body>
-</x-layout.layout>
+</x-layout.layout2>
