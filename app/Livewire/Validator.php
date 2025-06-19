67e98c6d40c3743e37f5c4f1b84ecb3efaf333ab
Jahmil <roy_animo@hotmail.com>
2025-06-19 22:57:37 +0200

    started on livewire form in validate.blade

--- app/Livewire/Validator.php
+++ app/Livewire/Validator.php
@@ -8,6 +8,11 @@ class Validator extends Component
 {
     public $liveName;
     public $liveGift;
+
+    public function save()
+    {
+        return redirect()->route('validateView')->with(session()->flash('status','Your gift request has ben sent.'));
+    }
     public function render()
     {
         return view('livewire.validator');
--- resources/views/livewire/validator.blade.php
+++ resources/views/livewire/validator.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div>
-        <div class="flex justify-center">
+        <div class="flex justify-center m-5">
             <label class="pt-1 mr-5" for="person">Naam collega:</label>
             <input type="text" name="person" class="border" wire:model.live="liveName">
         </div>
@@ -8,12 +8,12 @@
             <div class="w-1/3 p-2 flex justify-center">
                 <div>
                     <h1 class="font-bold">Verjaardagen:</h1>
-                    <select name="" id="">
-                        <option value="">30ste verjaardag</option>
-                        <option value="">40ste verjaardag</option>
-                        <option value="">50ste verjaardag</option>
-                        <option value="">60ste verjaardag</option>
-                        <option value="">65ste verjaardag</option>
+                    <select wire:model.live="liveGift">
+                        <option value="30ste verjaardag">30ste verjaardag</option>
+                        <option value="40ste verjaardag">40ste verjaardag</option>
+                        <option value="50ste verjaardag">50ste verjaardag</option>
+                        <option value="60ste verjaardag">60ste verjaardag</option>
+                        <option value="65ste verjaardag">65ste verjaardag</option>
                     </select>
                 </div>
             </div>
@@ -21,10 +21,10 @@
                 <div>
                     <div class="h-2/5">
                         <h1 class="font-bold">Huwelijk:</h1>
-                        <select name="" id="">
-                            <option value="">12,5 jaar huwelijk</option>
-                            <option value="">25 jaar huwelijk</option>
-                            <option value="">40 jaar huwelijk</option>
+                        <select wire:model.live="liveGift">
+                            <option value="12,5 jaar huwelijk">12,5 jaar huwelijk</option>
+                            <option value="25 jaar huwelijk">25 jaar huwelijk</option>
+                            <option value="40 jaar huwelijk">40 jaar huwelijk</option>
                         </select>
                     </div>
                     <br>
@@ -37,9 +37,9 @@
                 <div>
                     <h1 class="font-bold">Jaren ambtenaar:</h1>
                     <select wire:model.live="liveGift">
-                        <option value="12,5 jaar">12,5 jaar</option>
-                        <option value="25 jaar">25 jaar</option>
-                        <option value="40 jaar">40 jaar</option>
+                        <option value="12,5 jaar ambtenaar">12,5 jaar</option>
+                        <option value="25 jaar ambtenaar">25 jaar</option>
+                        <option value="40 jaar ambtenaar">40 jaar</option>
                     </select>
                 </div>
             </div>
@@ -54,19 +54,21 @@
                 <div class="text-center border border-gray-300">{{ $liveGift }}</div>
                 <div class="text-center border border-gray-300">-</div>
             </div>
-            <div class="grid grid-cols-3 p-5">
-                <div class="border border-gray-300 font-bold">Naam:</div>
-                <div class="border border-gray-300 font-bold">Gelegenheid:</div>
-                <div class="border border-gray-300 font-bold">Status:</div>
-                <div class="text-center border border-gray-300"></div>
-                <div class="text-center border border-gray-300">40 jaar huwelijk</div>
-                <div class="text-center border border-gray-300">afgewezen</div>
-                <div class="text-center border border-gray-300"></div>
-                <div class="text-center border border-gray-300">12,5 jaar ambtenaar</div>
-                <div class="text-center border border-gray-300">toegekend 12-5-2025</div>
-                <div class="text-center border border-gray-300"></div>
-                <div class="text-center border border-gray-300">30ste verjaardag</div>
-                <div class="text-center border border-gray-300">toegekend 17-6-2025</div>
+            <div class="mt-20">
+                <div class="grid grid-cols-3 p-5">
+                    <div class="border border-gray-300 font-bold">Naam:</div>
+                    <div class="border border-gray-300 font-bold">Gelegenheid:</div>
+                    <div class="border border-gray-300 font-bold">Status:</div>
+                    <div class="text-center border border-gray-300"></div>
+                    <div class="text-center border border-gray-300">40 jaar huwelijk</div>
+                    <div class="text-center border border-gray-300">afgewezen</div>
+                    <div class="text-center border border-gray-300"></div>
+                    <div class="text-center border border-gray-300">12,5 jaar ambtenaar</div>
+                    <div class="text-center border border-gray-300">toegekend 12-5-2025</div>
+                    <div class="text-center border border-gray-300"></div>
+                    <div class="text-center border border-gray-300">30ste verjaardag</div>
+                    <div class="text-center border border-gray-300">toegekend 17-6-2025</div>
+                </div>
             </div>
         </div>
     </div>
