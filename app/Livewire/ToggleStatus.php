4c4fe930e245ad0e3176493cf15d85e031d6011f
Jahmil <roy_animo@hotmail.com>
2025-10-17 00:18:09 +0200

    wire:click event in livewire

--- app/Livewire/ToggleStatus.php
+++ app/Livewire/ToggleStatus.php
@@ -8,6 +8,13 @@ class ToggleStatus extends Component
 {
     public $request;
 
+    public $buttonStatus = false;
+
+    public function buttonIsClicked()
+    {
+        $this->buttonStatus == false ? $this->buttonStatus = true : $this->buttonStatus = false;
+    }
+
     public function render()
     {
         return view('livewire.toggle-status');
--- resources/views/livewire/toggle-status.blade.php
+++ resources/views/livewire/toggle-status.blade.php
@@ -1,5 +1,5 @@
 @props(['request'])
 
 <div>
-    <div class="text-center border border-gray-300 hover:bg-almere hover:text-white hover:cursor-pointer">{{ $request }}</div>
+    <button class="w-full text-center border border-gray-300 hover:bg-almere hover:text-white hover:cursor-pointer" wire:click="buttonIsClicked">{{ $buttonStatus }}</button>
 </div>
