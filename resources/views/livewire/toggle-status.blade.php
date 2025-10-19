346c98ccdbf6aab59c3736e9a44d43ecb73b77db
Jahmil <roy_animo@hotmail.com>
2025-10-19 23:47:22 +0200

    making migrations for validation status

--- resources/views/livewire/toggle-status.blade.php
+++ resources/views/livewire/toggle-status.blade.php
@@ -1,5 +1,5 @@
 @props(['request'])
 
 <div>
-    <button class="{{ $buttonStatus ? 'bg-blue-100': 'bg-red-100'}}" wire:click="buttonIsClicked">{{ $request }}</button>
+    <button class="{{ $buttonStatus ? 'w-full text-center border border-gray-300 bg-orange-500 hover:bg-orange-200 text-white hover:cursor-pointer': 'w-full text-center border border-gray-300 hover:bg-almere hover:text-white hover:cursor-pointer'}}" wire:click="buttonIsClicked">{{ $buttonStatus ? 'goedgekeurd' : $request}}</button>
 </div>
