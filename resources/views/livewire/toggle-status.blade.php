0ec2c9d498b705e4f3702b69c30906fe5dad6ebc
Jahmil <roy_animo@hotmail.com>
2025-10-20 23:02:06 +0200

    working on flip effect onClick

--- resources/views/livewire/toggle-status.blade.php
+++ resources/views/livewire/toggle-status.blade.php
@@ -3,3 +3,6 @@
 <div>
     <button class="{{ $buttonStatus ? 'w-full text-center border border-gray-300 bg-orange-500 hover:bg-orange-200 text-white hover:cursor-pointer': 'w-full text-center border border-gray-300 hover:bg-almere hover:text-white hover:cursor-pointer'}}" wire:click="buttonIsClicked">{{ $buttonStatus ? 'goedgekeurd' : $request}}</button>
 </div>
+<!-- <div>
+    <button class="{{ $buttonStatus ? 'w-full text-center border border-gray-300 bg-orange-500 hover:bg-orange-200 text-white hover:cursor-pointer': 'w-full text-center border border-gray-300 hover:bg-almere hover:text-white hover:cursor-pointer'}}" wire:click="buttonIsClicked">{{ $buttonStatus ? 'goedgekeurd' : $request}}</button>
+</div> -->
