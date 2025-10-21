50a806e7745e2d713f27722bdeaa47cac03ee61b
Jahmil <roy_animo@hotmail.com>
2025-10-21 23:06:57 +0200

    forming the layout for card flip divs

--- resources/views/livewire/toggle-status.blade.php
+++ resources/views/livewire/toggle-status.blade.php
@@ -1,6 +1,14 @@
 @props(['request'])
 
-<div>
+<div class="container">
+    <div class="card">
+        <div class="card-front">
+
+        </div>
+        <div class="card-back">
+
+        </div>
+    </div>
     <button class="{{ $buttonStatus ? 'w-full text-center border border-gray-300 bg-orange-500 hover:bg-orange-200 text-white hover:cursor-pointer': 'w-full text-center border border-gray-300 hover:bg-almere hover:text-white hover:cursor-pointer'}}" wire:click="buttonIsClicked">{{ $buttonStatus ? 'goedgekeurd' : $request}}</button>
 </div>
 <!-- <div>
