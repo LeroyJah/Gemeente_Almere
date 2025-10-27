146a0366cf8e27b7748e48c67338664335e9bb1f
Jahmil <roy_animo@hotmail.com>
2025-10-27 23:49:19 +0100

    bug in group-hover effect

--- resources/views/livewire/toggle-status.blade.php
+++ resources/views/livewire/toggle-status.blade.php
@@ -5,11 +5,11 @@
     <!-- inner-card -->
     <div class="relative transition duration-800 hover:[transform:rotateY(180deg)]">
         <!-- front-card -->
-        <div class="absolute w-full text-center border border-gray-300 bg-orange-500 hover:bg-orange-200 text-white hover:cursor-pointer">
+        <div class="absolute w-full text-center border border-gray-300 bg-orange-500 text-white hover:cursor-pointer">
             {{ 'Goedgekeurd'}}
         </div>
         <!-- back-card -->
-        <div class="absolute w-full text-center border border-gray-300 hover:bg-almere hover:text-white hover:cursor-pointer">
+        <div class="absolute w-full text-center border border-gray-300 bg-almere text-white hover:cursor-pointer [transform:rotateY(180deg)] [backface-visibility:hidden]">
             {{ 'In Afwachting' }}
         </div>
     </div>
